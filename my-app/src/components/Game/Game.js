147e846b446fed835832bf905fd3ecc2c79e99dc
Markus Markowich <markus902@gmx.de>
2019-11-09 23:24:09 -0500

    working game logic

--- my-app/src/components/Game/Game.js
+++ my-app/src/components/Game/Game.js
@@ -4,25 +4,43 @@ import Nav from "../Nav";
 import images from "./ImageList.json";
 
 
-
 class Game extends Component {
     state = {
         images,
-        score: 0,
-        topScore: 0,
+        score: 1,
+        topScore: 1,
+        clicked: []
     }
 
-    handleClick = () => {
+    handleClick = (event) => {
+        let score = this.state.score;
+        let topScore = this.state.topScore;
+        let id = event.target.id;
+        let clicked = this.state.clicked;
 
-        let currentScore = this.state.score;
+        console.log(score, topScore);
 
-        currentScore++;
-        // increment Score
-        this.setState({ score: currentScore });
-        console.log(currentScore);
+        // shuffle image array
+        images.sort(function () { return .5 - Math.random(); });
 
-
-
+        if (clicked.indexOf(id)) {
+            clicked.push(id);
+            score++;
+            if (this.state.score >= this.state.topScore)
+                topScore++;
+        } else {
+            score = 1;
+            clicked = [];
+            this.setState({
+                score: 1,
+                clicked: []
+            })
+        }
+        this.setState({
+            clicked: clicked,
+            score: score,
+            topScore: topScore
+        })
     };
 
     render() {
@@ -37,6 +55,7 @@ class Game extends Component {
                                 id={elem.id}
                                 key={elem.id}
                                 handleClick={this.handleClick}
+                                id={elem.id}
                             />
                         ))
                     }
--- my-app/src/components/ImageCard/ImageCard.js
+++ my-app/src/components/ImageCard/ImageCard.js
@@ -4,7 +4,7 @@ import "./ImageCard.css";
 function ImageCard(props) {
     return (
         <div className="imageDiv" key={props.id}>
-            <img className="imageDiv" src={props.image} alt="" key={props.id} onClick={props.handleClick}></img>
+            <img className="imageDiv" src={props.image} alt="" key={props.id} onClick={props.handleClick} id={props.id}></img>
         </div>
     )
 }
