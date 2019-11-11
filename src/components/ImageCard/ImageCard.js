29eaae28ebd575a3bd1352bcb0ce600bfb77af89
Markus Markowich <markus902@gmx.de>
2019-11-10 23:24:45 -0500

    added styling

--- src/components/ImageCard/ImageCard.js
+++ src/components/ImageCard/ImageCard.js
@@ -4,7 +4,7 @@ import "./ImageCard.css";
 function ImageCard(props) {
     return (
         <div className="imageDiv" key={props.id}>
-            <img className="imageDiv" src={props.image} alt="" key={props.id} onClick={props.handleClick} id={props.id}></img>
+            <img className={props.wobble} src={props.image} alt="" key={props.id} onClick={props.handleClick} id={props.id}></img>
         </div>
     )
 }
